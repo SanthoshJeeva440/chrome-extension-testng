aa80d0d6d80ff1cf451924928377eeac98f31bf9
Santhosh <santhoshjeeva440@gmail.com>
2024-08-11 23:21:25 +0530

    add reports

--- src/test/java/smoke/TC_01.java
+++ src/test/java/smoke/TC_01.java
@@ -4,14 +4,16 @@ import org.calibraint.pages.HedgeHomePage;
 import org.testng.annotations.Test;
 import testSetup.SuiteSetup;
 
+
 public class TC_01 extends SuiteSetup {
-    @Test
-    public void validateHedgeHomePage(){
+    @Test(priority = 0)
+    public void validateHedgeHomePage() {
         HedgeHomePage.hedgeLaunchScreen();
     }
 
-    @Test
-    public void createNewWallet(){
-        HedgeHomePage.hedgeLaunchScreen();
+    @Test(priority = 1)
+    public void createNewWallet() {
+        HedgeHomePage.clickCreateWalletButton();
+
     }
 }
--- src/test/java/testSetup/SuiteSetup.java
+++ src/test/java/testSetup/SuiteSetup.java
@@ -1,9 +1,18 @@
 package testSetup;
 
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.service.ExtentTestManager;
+import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import org.calibraint.browser.Browser;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 
+@Listeners({ExtentITestListenerClassAdapter.class})
 public class SuiteSetup extends Browser {
 
     @BeforeClass(alwaysRun = true)
@@ -14,5 +23,24 @@ public class SuiteSetup extends Browser {
     @AfterClass()
     public static void closeBrowser() {
         driver.quit();
+    }
+
+    @AfterMethod
+    public synchronized void captureResult(ITestResult result) {
+        switch (result.getStatus()) {
+            case ITestResult.FAILURE:
+                ExtentTestManager.getTest(result).fail("ITestResult.FAILURE, event afterMethod",
+                        MediaEntityBuilder.createScreenCaptureFromBase64String(getImage()).build());
+                break;
+            case ITestResult.SKIP:
+                ExtentTestManager.getTest(result).skip("ITestResult.SKIP, event afterMethod");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private String getImage() {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BASE64);
     }
 }
